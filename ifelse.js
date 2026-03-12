8e5b91a422235817d45823eaca1eccd97b9b3e37
63010399499 <meghanavallamkondu28@gmail.com>
2026-03-12 12:23:38 +0530

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,7 @@
+let x=28;
+if (x>24)
+{
+    console.log("you can drive");
+}else{
+    console.log("you cannot drive");
+}
---
+++ or.js
@@ -0,0 +1,7 @@
+let age=30;
+let d=false
+if (age>18|| d==true){
+    console.log("you can dance");
+}else{
+    console.log("you cannot dance");
+}
