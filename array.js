415fa214d2140dd0af4b45e0c3ce0ac8bacad891
63010399499 <meghanavallamkondu28@gmail.com>
2026-03-13 11:11:10 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,3 @@
+let fruits=['mango','orange','apple'];
+console.log(fruits[0]);
+console.log(fruits[2]);
---
+++ day.js
@@ -0,0 +1,14 @@
+let day=10;
+switch (day){
+    case 1:console.log("monday");
+    break;
+    case 2:console.log("tues");
+    break;
+    case 3:console.log("wed");
+    break;
+    case 4:console.log("thru");
+    break;
+    case 5:console.log("fri");
+    break;
+    default :console.log("invalid input");
+}
